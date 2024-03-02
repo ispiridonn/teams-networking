d2bc864c0827057ed1296c645e1733ecb81ff7f2
Ionela <ionela.spiridon45@gmail.com>
2024-03-02 17:02:49 +0200

    renderTeams

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # teams-networking
+
+````sh
+pwd- imi arata in ce folder sunt
+f12- consola
+cd- ma mut cu un pas in spate
+start project- cd ../node-api apoi npm start
+```sh
+````
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import "./style.css";
-console.warn("app ready");
+
+function getTeamAsHTML(team) {
+  return `<tr>
+  <td>${team.promotion}</td>
+  <th>${team.members}</th>
+  <td>${team.name}</td>
+  <td>${team.url}</td>
+  <td>x</td>
+</tr>`;
+}
+
+function renderTeams(teams) {
+  //console.warn("render", teams);
+  const teamsHTML = teams.map(getTeamAsHTML);
+  //console.info(teamsHTML);
+  document.querySelector("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
 
 function loadTeams() {
   const promise = fetch("http://localhost:3000/teams-json")
     .then(r => r.json())
     .then(teams => {
-      console.table(teams);
-      return promise;
+      renderTeams(teams);
+      return teams;
     });
-  console.warn("loadTeams", promise);
 }
 
 loadTeams();
