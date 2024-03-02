12044409fedbde6ddbe05395213272011965a403
Ionela <ionela.spiridon45@gmail.com>
2024-03-02 12:03:06 +0200

    header layout ; static table ; load teams from API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./style.css";
 console.warn("app ready");
+
+function loadTeams() {
+  const a = fetch("http://localhost:3000/teams-json")
+    .then(r => r.json())
+    .then(teams => {
+      console.table(teams);
+      return teams;
+    });
+  console.warn("loadTeams", promise);
+}
+
+loadTeams();
