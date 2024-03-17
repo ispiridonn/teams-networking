da553471f347ca60652224cb9c4da2dd00716ab6
Ionela <ionela.spiridon45@gmail.com>
2024-03-17 12:49:10 +0200

    setFormValues

--- src/index.js
+++ src/index.js
@@ -76,6 +76,15 @@ function getFormValues() {
   }
 }
 
+function setFormValues(team) {
+  
+    $("input[name=promotion]").value = team.promotion;
+    $("input[name=members]").value = team.members;
+    $("input[name=name]").value = team.name;
+    $("input[name=url]").value = team.url;
+  
+}
+
 function onSubmit(e) {
   e.preventDefault();
   console.warn("pls save all values");
@@ -89,6 +98,7 @@ function startEdit(id) {
     return id === team.id;
    });
     console.warn("click on edit %o", id, )
+    setFormValues(team);
 }
 
 function initEvents() {
@@ -99,6 +109,7 @@ function initEvents() {
     deleteTeamRequest(id);
     window.location.reload();
   } else if ( e.target.matches("a.edit-btn")) {
+    e.preventDefault()''
    // const id= e.target.getAttributes("data-id");
    const id= e.target.dataset.id;
    startEdit(id);
