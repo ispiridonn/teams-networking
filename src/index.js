5e194f077c6efcca27318bf9ac0b4371ca127810
Ionela <ionela.spiridon45@gmail.com>
2024-03-17 11:07:44 +0200

    create updateTeamRequest

--- src/index.js
+++ src/index.js
@@ -18,8 +18,18 @@ function creatTeamRequest(team) {
   body: JSON.stringify({ id: id })
  });
  }
- 
 
+ function updateTeamRequest (team) {
+  fetch("http://localhost:3000/teams-json/update", {
+   method: "PUT",
+   headers: {
+    "Content-Type": "application/json"
+   },
+   body: JSON.stringify(team)
+ })
+ 
+ }
+ 
 function getTeamAsHTML(team) {
   return `<tr>
   <td>${team.promotion}</td>
