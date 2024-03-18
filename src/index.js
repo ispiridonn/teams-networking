5a26b9f0df66b87a2ff32f9f866e9e25ad92707b
Ionela <ionela.spiridon45@gmail.com>
2024-03-18 20:33:51 +0200

    use promise fron fetch to refresh page after update success

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function creatTeamRequest(team) {
  }
 
  function updateTeamRequest (team) {
-  fetch("http://localhost:3000/teams-json/update", {
+  return fetch("http://localhost:3000/teams-json/update", {
    method: "PUT",
    headers: {
     "Content-Type": "application/json"
@@ -92,12 +92,19 @@ function onSubmit(e) {
   let team = getFormValues();
   if (editId) {
     team.id = editId;
-    updateTeamRequest(team);
+    const req = updateTeamRequest(team);
+    const response = req.then(r => r.json());
+   response.then(status => {
+    if(status.success) {
+      window.location.reload();
+    }
+   })
   } else {
     createTeamRequest();
+    window.location.reload();
   }
   
-  window.location.reload();
+ 
 }
 
 function startEdit(teams, id) {
