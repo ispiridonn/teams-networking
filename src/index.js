ff8bc1722f0840f64b509488a586963bfbbd850e
Ionela <ionela.spiridon45@gmail.com>
2024-03-18 19:33:13 +0200

    improve startEdit to have more parametres(to be pur function)

--- src/index.js
+++ src/index.js
@@ -100,9 +100,9 @@ function onSubmit(e) {
   window.location.reload();
 }
 
-function startEdit(id) {
+function startEdit(teams, id) {
   editId = id;
-  const team = allTeams.find((team) => { 
+  const team = teams.find(team => { 
     return id === team.id;
    });
     console.warn("click on edit %o", id, )
